c3d2cb80dbc867ad1501907f654ebe5fbffc04aa
johnrobisoniv <john.robison.iv@gmail.com>
2016-02-13 13:10:50 -0700

    Rename powPull.py to powScrape.py

--- powScrape.py
+++ powScrape.py
@@ -1,3 +1,3 @@
-# Pull data from http://www.onthesnow.com/colorado/skireport.html
+# Scrape data from http://www.onthesnow.com/colorado/skireport.html
 # Extract table element in div class="resBox"
 # Store table in file on server
