324a20347d3d288c67a4fe1a85553312e382c978
johnrobisoniv <john.robison.iv@gmail.com>
2016-02-13 13:05:36 -0700

    Create powParse.py
    Parse through table data and create a python-friendly dictionary of resorts and new snow values.

---
+++ powParse.py
@@ -0,0 +1,3 @@
+# Opens file with OTS.com's "resBox" table data created by powPull.py
+# Parse through data - append relevant data and resort to dictionary?
+# Save dictionary as data file named resData-DATE.txt
