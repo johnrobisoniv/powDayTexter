c1fb43dbcd6c9cc16362b4e71941a2ef097e7e5d
johnrobisoniv <john.robison.iv@gmail.com>
2016-02-13 13:09:06 -0700

    Create powText.js
    Created file to check current pow data against user's pow preferences and send SMS if it's a pow day!

--- powText.js
+++ powText.js
@@ -1,1 +1,5 @@
-
+/*
+Compare results from powParse.py with shredderID preferences (resort, pow_limit, text_time)
+IF conditions are met, call Twilio API to send SMS to shredderID's phone number with message.
+How to "STOP" service?
+*/
